c98e68a16a8f0f4de675c27cdbf9298daed7d4cc
Jack Le <89977402+JackLe469@users.noreply.github.com>
2022-11-14 18:14:03 -0800

    Update preset.py

--- preset.py
+++ preset.py
@@ -1,45 +1,85 @@
-import dispense
-import display_preset_list
-import display_preset
-import make_preset
-import edit_preset
+from tkinter import *
+from tkinter import font
+import tkinter as tk
+from load_sensor_file import Load_sensor
+from hx711 import HX711
+from keypad_file_test import run_keypad_file
+import main_page
+import time
 
-# If display_choices ever returns a -1, manual.py will immediately return to main.py, restarting the program.
-EXIT = -1
+AMOUNT = -1
+CONTAINER = -1
+win = ''
 
-def run():
-    '''Main file calls this preset file. Runs the preset file.'''
 
-    # Displays preset recipe section by reading from microsd file.
-    # Preset layout... {preset#: 'compartment#-grams'}
-    presets = [
-        '1-30 2-40',
-        '3-15',
-        '1-20'
-    ]
+def start_motor_1(container, target_val, confirmation_screen):
+    global win
+    global pin
+    hx = HX711(19, 21)
+
+    # # dispensing = Frame(win)
+    # # dispensing.pack(fill='both', expand=1) 
+    # for widgets in confirmation_screen.winfo_children():
+    #     widgets.destroy()
+    # # widgets.after(1000, callback=None)
+    # label = Label(confirmation_screen, text='Loading...')
+    # label.pack(pady=20)
+    # b = tk.Button(confirmation_screen, text="CANCEL", command=lambda: sensor.kill_motor()).pack()
     
-    while True:
-        preset_choice = display_preset_list.run(presets)
-        if preset_choice == -1:
-            # Return back to main screen if user wishes to exit.
-            return
-        elif preset_choice == -2:
-            # Create a new preset
-            new_preset = make_preset.run()
-            presets.append(new_preset)
-            continue
+    if container == 1:
+        # hx, steps, target_val, motor_num
+        sensor = Load_sensor(hx, 30, target_val, 1, confirmation_screen, win)
+    elif container == 2:
+        sensor = Load_sensor(hx, -30, target_val, 1, confirmation_screen, win)
+    elif container == 3:
+        sensor = Load_sensor(hx, 30, target_val, 2, confirmation_screen, win)
+    elif container == 4:
+        sensor = Load_sensor(hx, -30, target_val, 2, confirmation_screen, win)
+    sensor.load_sensor()
+
+    # dispensing = Frame(win)
+    # dispensing.pack(fill='both', expand=1) 
+    # for widgets in confirmation_screen.winfo_children():
+    #     widgets.destroy()
+    # # widgets.after(1000, callback=None)
+    # label = Label(confirmation_screen, text='Loading...')
+    # label.pack(pady=20)
+    # b = tk.Button(confirmation_screen, text="CANCEL", command=lambda: sensor.kill_motor).pack()
+    # win.update()
 
 
-        # Ask user if they would like to dispense or edit the preset.
-        dispense_or_edit = display_preset.run(presets, preset_choice)
-
-        if dispense_or_edit == "dispense":
-            # Dispense chosen preset
-            dispense.run(presets[preset_choice])
-        elif dispense_or_edit == "edit":
-            # Edit chosen preset
-            new_preset = edit_preset.run(presets[preset_choice])
-            presets[preset_choice] = new_preset
+    for widgets in confirmation_screen.winfo_children():
+        widgets.destroy()
+    label = Label(confirmation_screen, text='Done dispensing ' + str(AMOUNT) + " grams from container " + str(CONTAINER) + "!")
+    label.pack(pady=20)
+    win.update()
+    pin=''
     
-    return
+    win.after(3000, confirmation_screen.pack_forget)
+    win.after(3000, main_page.run_main_page(win))
 
+
+def run_preset(plist, preset, confirmation_screen):
+    global AMOUNT
+    global CONTAINER
+    CONTAINER = plist[0]
+    AMOUNT = plist[1]
+
+    confirmation_screen.pack(fill='both', expand=1)
+    preset.pack_forget()
+    label = Label(confirmation_screen, text='Are you sure you would like to proceed with ' + str(AMOUNT) +" grams from compartment " + str(CONTAINER) + "?")
+    label.pack(pady=20)    
+    b = tk.Button(confirmation_screen, text="CONFIRM", command=lambda: start_motor_1(CONTAINER, int(AMOUNT), confirmation_screen)).pack()
+    label2 = Label(confirmation_screen, text='Cake Helper will tare once amount is confirmed. Please be patient.')
+    label2.pack(pady=20) 
+
+
+def run_preset_page(window_from_start, preset, keypad, font1):
+    global win
+    win = window_from_start
+    confirmation_screen = Frame(window_from_start)
+
+    label1 = Label(preset, text="Choose a preset", foreground="black", font=font1)
+    label1.pack(pady=10)
+    btn_start = Button(preset, text="Preset 1", command=lambda: run_preset([2, 20], preset, confirmation_screen))
+    btn_start.pack(pady=10)
